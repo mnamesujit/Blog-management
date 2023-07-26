8ddbeefec923fbcf3f1b87a4d058abe130621580
Sujit <mnamesujit@gmail.com>
2023-07-26 13:18:53 +0530

    fix: added back to home button on IOT page

--- blog.js
+++ blog.js
@@ -1,13 +1,17 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
+let mybutton2 = document.getElementById("iot-back-button");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  console.log("called")
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    mybutton2.style.display = "block";
   } else {
     mybutton.style.display = "none";
+    mybutton2.style.display = "none";
   }
 }
